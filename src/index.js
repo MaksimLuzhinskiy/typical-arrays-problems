116e3cb793ddac4e00504c95620fbadffa709494
MaksimLuzhinskiy <maks.luzhinskiy00@gmail.com>
2021-02-16 21:15:55 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(!array || array.length == 0){return 0}
+  let min_h =array[0];
+  array.map((x)=>{
+      x < min_h ? min_h=x : x; 
+  });
+  return min_h;
 }
-
 exports.max = function max (array) {
-  return 0;
+  if(!array || array.length == 0){return 0}
+  let max =0;
+  array.map((x)=>{
+      x > max ? max=x : x; 
+  });
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!array || array.length == 0){return 0}
+  let avg =0;
+  array.map((x)=>{
+    avg+=x;
+  });
+  return avg/array.length;
 }
